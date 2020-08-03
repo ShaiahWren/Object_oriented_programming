8b122b2447762875fd91dcf670c8ecd32378663b
Shaiah Wren <sde2111@columbia.edu>
2020-08-02 21:53:25 -0400

    notes about oop

--- notes.py
+++ notes.py
@@ -70,4 +70,58 @@ class Pet:
         self.fullness -= 5
         self.happiness -+ 5
 
+# benji = Pet("Benji", 50, 20, 20, 1)
+# lassie = Pet("Lassie", 50, 20, 20, 1)
+# clifford = Pet("Old Yeller", 50, 20, 20, 1)
 
+
+## Inheritance
+
+class CuddlyPet(Pet):
+    pass
+
+benji = CuddlyPet("Benji", 50, 20)
+print(benji.fullness, benji.happiness) ##50 20
+# benji.be_alive()
+print(benji.fullness, benji.happiness)
+
+# from oop_16 import Pet  
+# class CuddlyPet(Pet):
+#     def cuddle(self, other_pet):
+#         other_pet.get_love()
+
+
+## Polymorphism
+
+class Pet:
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5):
+        self.name = name
+        self.fullness = fullness
+        self.happiness = happiness
+        self.hunger = hunger
+        self.mopiness = mopiness
+
+    def eat_food(self):
+        self.fullness += 30
+
+    def get_love(self):
+        self.happiness += 30
+
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness
+
+class CuddlyPet(Pet):
+    def __init__(self, name, fullness=50, hunger=5, cuddle_level=1):
+        super().__init__(name, fullness, 100, hunger, 1)
+        self.cuddle_level = cuddle_level
+
+    def be_alive(self):
+        self.fullness -= self.hunger
+        self.happiness -= self.mopiness/2
+
+    def cuddle(self, other_pet):
+        #super cuddle powers, activate!
+        for i in range(self.cuddle_level):
+            other_pet.get_love()
+    

b5fe7506e4fbb3f65008ffa7f9b2e5ce0501f197
Shaiah Wren <sde2111@columbia.edu>
2020-08-04 12:06:58 -0400

    string methods

--- module_exerciese/small_1.py
+++ module_exerciese/small_1.py
@@ -7,22 +7,46 @@ class Pet:
         self.mopiness = mopiness
         self.toys = []
 
+    def __str__(self):
+        return """
+            %s:
+            Fullness: %d
+            Happiness: %d
+            Hunger: %d
+            Mopiness: %d
+        """ % (self.name, self.fullness, self.happiness, self.hunger, self.mopiness)
+
     def be_alive(self):
         self.fullness -= self.hunger
         self.happiness -= self.mopiness
-        for toy in self.toys:
-            self.happiness += toy.use()
-
+        print("Be alive in Pet class for %s" % self.name)
+ 
 
 
 class CuddlyPet(Pet):   
+    def __init__(self, name, fullness=50, happiness=50, hunger=5, mopiness=5, cuddle_level=1):
+        super().__init__(name, fullness=fullness, happiness=happiness, hunger=hunger, mopiness=mopiness)
+        self.cuddle_level = cuddle_level  
+
+    def __str__(self):
+        return """
+        %s: 
+        Fullness: %d
+        Happiness: %d
+        Hunger: %d
+        Mopiness: %d
+        Cuddle level: %s
+        """ % (self.name, self.fullness, self.happiness, self.hunger, self.mopiness, self.cuddle_level)
+
     def be_alive(self):
         super().be_alive()
         self.happiness -= self.mopiness/2
-        
+        print("Be alive in CuddlyPet class for %s" % self.name)
 
-    def __str__(self):
-        return "%s is extra cuddly" % self.name
+# print(CuddlyPet("Harriet", 50, 100, 30, 0, "Extra cuddly"))
 
+guster= Pet("Guster", 10, 100, 200, 0)
+beans = CuddlyPet("Beans", 20, 100, 200, 0, "Extra Cuddly")
 
-print(CuddlyPet("Harriet"))
+guster.be_alive()
+beans.be_alive()
